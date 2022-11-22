63bf76307e732048fb3e6d07a7590b42b84ce0c8
sylvainboyer <sylvainboyer@airdeclementine.lan>
2022-11-22 15:23:00 +0100

    v1

---
+++ src/petitPrince/PetitPrince0.java
@@ -0,0 +1,34 @@
+package petitPrince;
+
+import asteroide.Sujet;
+import asteroide.console.ConsoleJavaBoy;
+
+public class PetitPrince0 implements Sujet{
+	private String nom;
+	private int argent;
+	private ConsoleJavaBoy console;
+	private int cpt;
+	
+	public PetitPrince0() {
+		
+	}
+	
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int getArgent() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
